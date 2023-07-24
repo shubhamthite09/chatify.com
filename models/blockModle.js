a0f173755f41fb0a38bfc6e49f50395256db596d
Thite Shubham <115460300+shubhamthite09@users.noreply.github.com>
2023-07-24 18:44:28 +0530

    all formated

--- models/blockModle.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = mongoose.Schema({
-    email:{type:String,required:true},
-});
-
-const blockModel = mongoose.model("blockedUser", userSchema);
-
-module.exports = { blockModel };
